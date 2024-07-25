b533bc6f19aad05f3454cf7607b6d9bc78b5d755
Arrizki0098 <akbar.rabbani@iconpln.co.id>
2024-07-25 21:30:50 +0700

    Redeploy

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,11 @@
-import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+      '@': '/src',
+    },
   },
-  build: {
-    outDir: 'dist', // Direktori output untuk build
-    sourcemap: true, // Menghasilkan sourcemap untuk debugging
-  },
-  server: {
-    port: 3000, // Port untuk development server
-    open: true, // Membuka browser secara otomatis
-  }
-})
+});
